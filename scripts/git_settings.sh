d3f4f8dd4908793149a2c9c74c0f440c8608d495
jpsern <tommy999_jps@yahoo.co.jp>
2021-03-17 17:11:08 +0900

    初期設定コマンドを復帰

--- scripts/git_settings.sh
+++ scripts/git_settings.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo $REPOSITORY_OWNER
-echo $REPOSITORY_OWNER_EMAIL
-echo $(echo ${GITHUB_REF#refs/heads/})
-# git config --local user.email ${{ secrets.REPOSITORY_OWNER_EMAIL }}
-# git config --local user.name ${{ github.repository_owner }}
-# git checkout $(echo ${GITHUB_REF#refs/heads/}) 
+git config --local user.email $REPOSITORY_OWNER_EMAIL
+git config --local user.name $REPOSITORY_OWNER
+git checkout $(echo ${GITHUB_REF#refs/heads/})
