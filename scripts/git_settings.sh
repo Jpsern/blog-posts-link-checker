ade0eaadfa8d19385410efa6bef7a3e74895d802
jpsern <tommy999_jps@yahoo.co.jp>
2021-03-17 17:02:02 +0900

    secretsだけがNGなのかの検証

--- scripts/git_settings.sh
+++ scripts/git_settings.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git config --local user.email ${{ secrets.REPOSITORY_OWNER_EMAIL }}
-git config --local user.name ${{ github.repository_owner }}
+echo ${{ github.repository_owner }}
+# git config --local user.email ${{ secrets.REPOSITORY_OWNER_EMAIL }}
+# git config --local user.name ${{ github.repository_owner }}
 # git checkout $(echo ${GITHUB_REF#refs/heads/}) 
