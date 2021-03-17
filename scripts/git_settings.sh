f051f8a79c74a3a225cbeaadd18e52b06afc0005
jpsern <tommy999_jps@yahoo.co.jp>
2021-03-17 16:51:54 +0900

    切り分けのためgit status のみ出力

--- scripts/git_settings.sh
+++ scripts/git_settings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git config --local user.email ${{ secrets.REPOSITORY_OWNER_EMAIL }}
-git config --local user.name ${{ github.repository_owner }}
-git fetch origin
-git checkout $(echo ${GITHUB_REF#refs/heads/}) 
+git status
+# git config --local user.email ${{ secrets.REPOSITORY_OWNER_EMAIL }}
+# git config --local user.name ${{ github.repository_owner }}
+# git checkout $(echo ${GITHUB_REF#refs/heads/}) 
