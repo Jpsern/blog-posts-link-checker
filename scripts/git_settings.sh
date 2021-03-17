c2d8250636bf44139a5e26f791e9e9d896981d87
jpsern <tommy999_jps@yahoo.co.jp>
2021-03-17 17:04:57 +0900

    envからならどうか確認

--- scripts/git_settings.sh
+++ scripts/git_settings.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo ${{ github.repository_owner }}
+echo $REPOSITORY_OWNER
+echo $REPOSITORY_OWNER_EMAIL
+echo $(echo ${GITHUB_REF#refs/heads/})
 # git config --local user.email ${{ secrets.REPOSITORY_OWNER_EMAIL }}
 # git config --local user.name ${{ github.repository_owner }}
 # git checkout $(echo ${GITHUB_REF#refs/heads/}) 
