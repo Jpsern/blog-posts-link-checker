d79e8cd16c8f25516c8187c4c4cf022673966317
jpsern <tommy999_jps@yahoo.co.jp>
2021-03-17 16:54:45 +0900

    statusだけなら問題なかったのでconfigまで実行

--- scripts/git_settings.sh
+++ scripts/git_settings.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git status
-# git config --local user.email ${{ secrets.REPOSITORY_OWNER_EMAIL }}
-# git config --local user.name ${{ github.repository_owner }}
+git config --local user.email ${{ secrets.REPOSITORY_OWNER_EMAIL }}
+git config --local user.name ${{ github.repository_owner }}
 # git checkout $(echo ${GITHUB_REF#refs/heads/}) 
