78b32ca924dab0bbf6ea76614ba4fbca2cf9d5ee
jpsern <tommy999_jps@yahoo.co.jp>
2021-03-17 16:40:42 +0900

    Git初期設定用のシェルを作成

---
+++ scripts/git_settings.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --local user.email ${{ secrets.REPOSITORY_OWNER_EMAIL }}
+git config --local user.name ${{ github.repository_owner }}
+git fetch origin
+git checkout $(echo ${GITHUB_REF#refs/heads/}) 
