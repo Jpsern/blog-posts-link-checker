25853f003cc72d7f6ba444968e485af283e5610a
jpsern <tommy999_jps@yahoo.co.jp>
2021-03-24 01:22:39 +0900

    envの記法ミス修正

--- .github/scripts/create-issue.js
+++ .github/scripts/create-issue.js
@@ -1,7 +1,10 @@
 const octokit = new Octokit();
+const owner = "Jpsern";
+const repo = "test-product";
+
 const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
-  owner: "Jpsern",
-  repo: "test-product",
+  owner,
+  repo,
   title: "My test issue",
 });
 console.log("Issue created: %s", data.html_url);
