16902c5274f3b3177437fe53a158ee378a8cbba4
jpsern <tommy999_jps@yahoo.co.jp>
2021-03-24 01:34:03 +0900

    syntax error 対応
    下記エラーが出るため、asyncのスコープ内に入れた
    await is only valid in async function

--- .github/scripts/create-issue.js
+++ .github/scripts/create-issue.js
@@ -2,9 +2,11 @@ const octokit = new Octokit();
 const owner = "Jpsern";
 const repo = "test-product";
 
-const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
-  owner,
-  repo,
-  title: "My test issue",
-});
-console.log("Issue created: %s", data.html_url);
+async () => {
+  const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+    owner,
+    repo,
+    title: "My test issue",
+  });
+  console.log("Issue created: %s", data.html_url);
+}
