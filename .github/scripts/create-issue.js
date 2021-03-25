4bc62878cdd7ce3956c4d2f2a9e1c195a267084e
jpsern <tommy999_jps@yahoo.co.jp>
2021-03-26 01:36:34 +0900

    title修正

--- .github/scripts/create-issue.js
+++ .github/scripts/create-issue.js
@@ -5,11 +5,10 @@ const [ owner ] = process.env.GITHUB_REPOSITORY.split("/");
 const repo = "test-product";
 
 (async () => {
-  console.log("execute async...");
   const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
     owner,
     repo,
-    title: `Found broken link(${process.env.CURRENT_DATE})`,
+    title: `Fix broken links. (${process.env.CURRENT_DATE})`,
     body: "https://github.com/Jpsern/blog-posts-link-checker/blob/master/link-check-result.txt",
     labels: ['bug']
   });
