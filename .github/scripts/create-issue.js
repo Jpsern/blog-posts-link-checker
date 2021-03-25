e7b34f4ed62a2f01dc8d5e3568eaa95b2e19ebea
jpsern <tommy999_jps@yahoo.co.jp>
2021-03-26 01:23:59 +0900

    本文とラベルを追加した状態で再度試してみる

--- .github/scripts/create-issue.js
+++ .github/scripts/create-issue.js
@@ -1,15 +1,17 @@
 const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 const [ owner ] = process.env.GITHUB_REPOSITORY.split("/");
-const repo = process.env.TARGET_REPO;
+// const repo = process.env.TARGET_REPO;
+const repo = "test-product";
 
 (async () => {
   console.log("execute async...");
-  console.log([owner, repo]);
-  // const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
-  //   owner,
-  //   repo,
-  //   title: "My test issue",
-  // });
-  // console.log("Issue created: %s", data.html_url);
+  const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+    owner,
+    repo,
+    title: "Found broken link -" + process.env.CURRENT_DATE,
+    body: "https://github.com/Jpsern/blog-posts-link-checker/blob/master/link-check-result.txt",
+    labels: ['bug']
+  });
+  console.log("Issue created: %s", data.html_url);
 })();
