79b20567909c4c94e3987e5e905cc24a5de97216
TOMMY(a.k.a JPS.) <tommy999_jps@yahoo.co.jp>
2021-03-26 01:52:41 +0900

    リンクチェック実行後に Issue を発行する (#7)
    * .github内にscriptsディレクトリを作成
    
    * octokit/actionのインストール
    
    * issue発行用のjsを作成
    
    * issue発行用の試験的なワークフロー作成
    
    * templateエラー修正
    
    * envの記法ミス修正
    
    * syntax error 対応
    
    下記エラーが出るため、asyncのスコープ内に入れた
    await is only valid in async function
    
    * import忘れ
    
    * コピペの残骸
    
    * debug用console.log追加
    
    * async実行してなかった
    
    即時関数書いたつもりがただの定義だった
    
    * リポジトリ依存かの切り分け
    
    * tokenを差し替え
    
    * 元に戻してみる
    
    * privateでも動くか検証
    
    * issueは発行できたので一旦コメントアウト
    
    * 本文とラベルを追加した状態で再度試してみる
    
    * dateがとれてなかったのを修正
    
    * title修正
    
    * title修正
    
    * body修正
    
    * body修正
    
    * link-check用CIにIssue発行機能を組み込み
    
    * pushトリガーは不要になったので削除

---
+++ .github/scripts/create-issue.js
@@ -0,0 +1,15 @@
+const { Octokit } = require("@octokit/action");
+const octokit = new Octokit();
+const [ owner ] = process.env.GITHUB_REPOSITORY.split("/");
+const repo = process.env.TARGET_REPO;
+
+(async () => {
+  const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+    owner,
+    repo,
+    title: `Fix broken links. (${process.env.CURRENT_DATE})`,
+    body: "# Details\nPlease refer to the link below.\nhttps://github.com/Jpsern/blog-posts-link-checker/blob/master/link-check-result.txt",
+    labels: ['bug']
+  });
+  console.log("Issue created: %s", data.html_url);
+})();
