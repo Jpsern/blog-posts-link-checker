7793424578b0237713f3add1a23b11bc3181c09f
jpsern <tommy999_jps@yahoo.co.jp>
2021-03-26 01:48:26 +0900

    link-check用CIにIssue発行機能を組み込み

--- .github/scripts/create-issue.js
+++ .github/scripts/create-issue.js
@@ -1,8 +1,7 @@
 const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 const [ owner ] = process.env.GITHUB_REPOSITORY.split("/");
-// const repo = process.env.TARGET_REPO;
-const repo = "test-product";
+const repo = process.env.TARGET_REPO;
 
 (async () => {
   const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
