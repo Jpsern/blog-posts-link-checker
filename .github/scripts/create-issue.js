99a9c4edc8eb8b4e83ff0c38f1134b445095b2ff
TOMMY(a.k.a JPS.) <tommy999_jps@yahoo.co.jp>
2021-05-02 23:58:12 +0900

    Update create-issue.js

--- .github/scripts/create-issue.js
+++ .github/scripts/create-issue.js
@@ -9,6 +9,7 @@ const repo = process.env.TARGET_REPO;
     repo,
     title: `Found broken links. (${process.env.CURRENT_DATE})`,
     body: "# Details\nPlease refer to the link below.\nhttps://github.com/Jpsern/blog-posts-link-checker/blob/master/link-check-result.txt",
+    assignee: owner
     labels: ['bug']
   });
 })();
