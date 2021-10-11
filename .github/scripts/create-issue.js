f2b1c3e10a26e1e3ffc89693ed03030886d5cadf
TOMMY(a.k.a JPS.) <tommy999_jps@yahoo.co.jp>
2021-10-12 04:54:51 +0900

    リンクチェックの結果をmarkdown化 (#25)
    * wip
    
    * front matter部分を作成
    
    * ヘディング追加
    
    * バックスラッシュ不足
    
    * 一部整形
    
    * 整形中
    
    * 各行の頭に`*`を付けたいのでsedコマンドを使う方法に変更
    
    * 正常終了時のメッセージを追加
    
    * 微調整と不要コメントの削除
    
    * ファイル拡張子の変更に追従
    
    * ワークフローのテスト実行
    
    * Update link-check-result.md
    
    * リダイレクトミスってたので修正
    
    * Update link-check-result.md
    
    * if文の条件逆だった
    
    * もう1箇所リダイレクトミス発見
    
    * 凡ミス修正
    
    qつけわすれた
    
    * Update link-check-result.md
    
    * リダイレクト再修正
    
    ヘディング出力してなかった
    
    * ヘディングが出力されていない原因調査のため一旦コメントアウト
    
    リンクチェックしていると時間を食うので原因がわかるまでこの状態にしておく
    
    * コメントアウト忘れ
    
    * 大丈夫そうなので元に戻す
    
    * Update link-check-result.md
    
    * テスト用の記述を削除
    
    * テキストファイルに今後はマークダウンに移行した旨を追記

--- .github/scripts/create-issue.js
+++ .github/scripts/create-issue.js
@@ -8,7 +8,7 @@ const repo = process.env.TARGET_REPO;
     owner,
     repo,
     title: `Found broken links. (${process.env.CURRENT_DATE})`,
-    body: "# Details\nPlease refer to the link below.\nhttps://github.com/Jpsern/blog-posts-link-checker/blob/master/link-check-result.txt",
+    body: "# Details\nPlease refer to the link below.\nhttps://github.com/Jpsern/blog-posts-link-checker/blob/master/link-check-result.md",
     assignee: owner,
     labels: ['bug']
   });
---
+++ link-check-result.md
@@ -0,0 +1,5 @@
+---
+Checked date: 2021-10-12 04:43:23(+0900)
+---
+# Link check result
+All external links operational
