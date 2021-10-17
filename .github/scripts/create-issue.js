ad63ac7c3fefc44160185a0880530e41e0120a28
Jpsern <tommy999_jps@yahoo.co.jp>
2021-10-18 02:02:45 +0900

    Issueの本文にmdファイルの中身を挿入する

--- .github/scripts/create-issue.js
+++ .github/scripts/create-issue.js
@@ -2,13 +2,14 @@ const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 const [ owner ] = process.env.GITHUB_REPOSITORY.split("/");
 const repo = process.env.TARGET_REPO;
+const fs = require("fs");
 
 (async () => {
   const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
     owner,
     repo,
     title: `Found broken links. (${process.env.CURRENT_DATE})`,
-    body: "# Details\nPlease refer to the link below.\nhttps://github.com/Jpsern/blog-posts-link-checker/blob/master/link-check-result.md",
+    body: fs.readFileSync("link-check-result.md", "utf-8"),
     assignee: owner,
     labels: ['bug', 'brokenlinks']
   });
