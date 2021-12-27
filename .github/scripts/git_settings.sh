f59647dc482047c54c71c6b2378ba94c0fec96b0
TOMMY a.k.a. JPS <tommy999_jps@yahoo.co.jp>
2021-12-28 00:05:20 +0900

    git_setting.sh の改善 (#48)
    * wip
    
    * 環境変数を見て処理を分岐するように変更
    
    名前付き引数みたいなのができればいいが
    できないので環境変数を指定して実行される前提で処理を変更
    
    * extend.yml に試験的に組み込み
    
    * 環境変数の指定を外す
    
    * コメント補足
    
    * 処理を差し替えたので不要な記述を除外
    
    * テスト用の記述が残っていたので消す

--- .github/scripts/git_settings.sh
+++ .github/scripts/git_settings.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-git config --local user.email $REPOSITORY_OWNER_EMAIL
-git config --local user.name $REPOSITORY_OWNER
+set -e
+
+if [ -z "$USER_NAME" ] && [ -z "$USER_EMAIL" ]; then
+    # github-actions bot としてコミットするための設定
+    # https://github.com/actions/checkout/discussions/479
+    USER_NAME="github-actions[bot]" 
+    USER_EMAIL="github-actions[bot]@users.noreply.github.com" 
+elif [ -z "$USER_EMAIL" ] || [ -z "$USER_NAME" ]; then
+    echo 'Not enough settings'
+    exit 1
+fi
+
+git config --local user.email $USER_EMAIL
+git config --local user.name $USER_NAME
 git fetch origin
 git checkout $(echo ${GITHUB_REF#refs/heads/})
 git pull
