3601db70591d6552b81935a2510ba7c89a70106d
Jpsern <tommy999_jps@yahoo.co.jp>
2022-08-12 10:34:48 +0900

    最新Issueの番号がわからないのでAPIで取得するように変更

--- .github/scripts/chatwork.js
+++ .github/scripts/chatwork.js
@@ -2,11 +2,24 @@ const { URLSearchParams } = require('url');
 const fetch = require('node-fetch');
 const encodedParams = new URLSearchParams();
 const token = process.env.CHATWORK_TOKEN;
-const issueUrl = process.env.ISSUE_URL;
+const getLatestIssue = async () => {
+    const { Octokit } = require("@octokit/action");
+    const octokit = new Octokit();
+    const [ owner ] = process.env.GITHUB_REPOSITORY.split("/");
+    const repo = process.env.TARGET_REPO;
+    const { data } = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+        owner,
+        repo,
+        state: 'open',
+        labels: ['bug', 'brokenlinks']
+    });
+    const issue = data[0];
+    return issue['html_url'];
+}
 const message =
 `[toall][info][title]リンクチェック結果[/title]
 リンクエラーが見つかりました。下記を確認してください。
-${issueUrl}[/info]`;
+${getLatestIssue()}[/info]`;
 
 (() => {
     encodedParams.set('self_unread', '1'); // 既読にはしない
