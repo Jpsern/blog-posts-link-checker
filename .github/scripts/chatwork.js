98a256b11171911236322aaade0b21639664132c
TOMMY a.k.a. JPS <tommy999_jps@yahoo.co.jp>
2022-08-16 23:28:00 +0900

    IssueのURLが表示されていなかったのを修正 (#77)
    * outdent追加
    
    * issueのURLが表示されていなかったのを修正

--- .github/scripts/chatwork.js
+++ .github/scripts/chatwork.js
@@ -1,8 +1,9 @@
 import { URLSearchParams } from 'url';
 import fetch from 'node-fetch';
 import { Octokit } from '@octokit/action';
+import outdent from 'outdent';
 
-const getLatestIssue = async () => {
+(async () => {
     const octokit = new Octokit();
     const [ owner ] = process.env.GITHUB_REPOSITORY.split("/");
     const repo = process.env.TARGET_REPO;
@@ -12,15 +13,12 @@ const getLatestIssue = async () => {
         state: 'open',
         labels: ['bug', 'brokenlinks']
     });
-    const issue = data[0];
-    return issue['html_url'];
-}
-const message =
-`[toall][info][title]リンクチェック結果[/title]
-リンクエラーが見つかりました。下記を確認してください。
-${getLatestIssue()}[/info]`;
-
-(() => {
+    const message = outdent`
+        [toall][info][title]リンクチェック結果[/title]
+        リンクエラーが見つかりました。下記を確認してください。
+        ${data[0]['html_url']}[/info]
+    `;
+    
     const encodedParams = new URLSearchParams();
     encodedParams.set('self_unread', '1'); // 既読にはしない
     encodedParams.set('body', message);
